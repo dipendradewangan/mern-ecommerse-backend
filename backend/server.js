b6f617d19762e3e698e46454d378066a33c58d99
dipendra dewangan <dewangandipendradewangan@gmail.com>
2023-04-12 10:36:26 +0530

    connected to the database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
 const server = require("./app");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-
+// env file configuration
 dotenv.config({path : "backend/config/config.env"});
+
+
+// connect to the database
+mongoose.connect(process.env.DB_URL).then((data)=>{
+    console.log("Database successfully connected with the server")
+}).catch((err)=>{
+    console.log(err)
+})
 
 
 server.listen(process.env.PORT, (req, res)=>{
