810ea68e2e89bd4a28479acb693cf6fbabe46d78
dipendra dewangan <dewangandipendradewangan@gmail.com>
2023-04-12 11:31:02 +0530

    mongodb database product schema created

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,19 +1,26 @@
-const getAllProducts = (req, res)=>{
-    console.log(req.body)
+const ProductSchema = require("../model/productModel");
+
+const getAllProducts = async (req, res)=>{
+    
+    const productCollection = await ProductSchema.find()
     res.status(200).json({
         success : true,
-        message : "product found",
-        request : "put request"
+        message : "Data Found!",
+        data : productCollection
     })
 }
 
 
-const createProduct = (req, res)=>{
+const createProduct = async (req, res)=>{
     console.log(req.body)
+
+    const collection = new ProductSchema(req.body);
+    const product = await collection.save();
+
     res.status(200).json({
         success : true,
-        message : "product found",
-        request : "delete request"
+        message : "product created",
+        data : product
     })
 }
 
