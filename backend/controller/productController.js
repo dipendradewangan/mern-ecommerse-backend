08e6e4878107301c90b552303c6a1b317a9b6dd8
dipendra dewangan <dewangandipendradewangan@gmail.com>
2023-04-13 10:57:25 +0530

    created get post delete and put controller for crud operation

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,5 +1,8 @@
+
 const ProductSchema = require("../model/productModel");
 
+
+// controlles for fetch all product
 const getAllProducts = async (req, res)=>{
     
     const productCollection = await ProductSchema.find()
@@ -11,35 +14,68 @@ const getAllProducts = async (req, res)=>{
 }
 
 
+
+// controlles for fetch create a product
 const createProduct = async (req, res)=>{
-    console.log(req.body)
-
     const collection = new ProductSchema(req.body);
-    const product = await collection.save();
+    try{
+        const product = await collection.save();
+        res.status(201).json({
+            success : true,
+            message : "Product created!",
+            data : product
+        })
 
-    res.status(200).json({
-        success : true,
-        message : "product created",
-        data : product
-    })
+    }
+    catch(err){
+        res.status(500).json({
+            success : false,
+            message : err
+        })
+    }
 }
 
-const updateProduct = (req, res)=>{
-    console.log(req.body)
+// controlles for fetch update a selected product
+const updateProduct = async (req, res)=>{
+    const id = req.params.id;
+    const product = await ProductSchema.findById(id)
+
+    if(!product){
+
+        res.status(404).json({
+            success : false,
+            message : "product not found",
+        })
+    }
+    
+    const updatedProduct = await ProductSchema.findByIdAndUpdate(id, req.body)
     res.status(200).json({
         success : true,
-        message : "product found",
-        request : "put request"
+        message : "Product successfully updated",
+        data : updatedProduct
     })
+    
 }
 
-const deleteProduct = (req, res)=>{
-    console.log(req.body)
+const deleteProduct = async (req, res)=>{
+    const id = req.params.id;
+    const product = await ProductSchema.findById(id)
+    
+    if(!product){
+    
+        res.status(404).json({
+            success : false,
+            message : "product not found",
+        })
+    }
+    
+    const deletedProduct = await ProductSchema.findByIdAndDelete(id, req.body)
     res.status(200).json({
         success : true,
-        message : "product found",
-        request : "delete request"
+        message : "Product successfully deleted",
+        data : deletedProduct
     })
+  
 }
 
 module.exports = {
