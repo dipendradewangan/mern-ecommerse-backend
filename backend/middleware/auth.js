5145e0798134e5cd579c43ee0978d189a85b9c08
dipendra dewangan <dewangandipendradewangan@gmail.com>
2023-04-18 22:30:54 +0530

    created authorised role for protected as a specific role to handle protected work@

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,7 +3,7 @@ const ErrorHandler = require("../utils/ErrorHandler");
 const jwt = require("jsonwebtoken");
 const userSchema = require("../model/userModel");
 
-
+// user loged in then work any modules
 const isAuthenticateUser = catchAsyncError(async (req, res, next) => {
     const { token } = req.cookies;
     if (!token) {
@@ -15,4 +15,23 @@ const isAuthenticateUser = catchAsyncError(async (req, res, next) => {
     next();
 })
 
-module.exports = isAuthenticateUser; 
+
+// user any specific roles then work any module
+const authorisedRoles = (...roles)=>{
+
+    return (req, res, next)=>{
+        if(!roles.includes(req.user.role)){
+            return next(new ErrorHandler(`Role : ${req.user.role} is not allowed to access this resourse!`, 401))
+        }
+        console.log(req.user.role)
+        next();
+    }
+}
+
+
+
+
+module.exports = {
+    isAuthenticateUser,
+    authorisedRoles
+}; 
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,21 +1,21 @@
 const router = require("express").Router();
 const { getAllProducts, updateProduct, createProduct, deleteProduct, getProductDetails } = require("../controller/productController");
-const isAuthenticateUser = require("../middleware/auth");
+const { isAuthenticateUser, authorisedRoles } = require("../middleware/auth");
 
 
 // route for get all products
-router.get("/products", isAuthenticateUser, getAllProducts)
+router.get("/products", isAuthenticateUser, authorisedRoles("admin",), getAllProducts)
 
 // route for get any product Details using product id
 router.get("/product/:id", getProductDetails)
 
 // route for creat a product
-router.post("/product/new", createProduct)
+router.post("/product/new", isAuthenticateUser, authorisedRoles(), createProduct)
 
 // route for update to selected product
-router.put("/product/:id", updateProduct)
+router.put("/product/:id", isAuthenticateUser, authorisedRoles(), updateProduct)
 
 // route for delete to selected product
-router.delete("/product/:id", deleteProduct)
+router.delete("/product/:id", isAuthenticateUser,authorisedRoles(), deleteProduct)
 
 module.exports = router;
