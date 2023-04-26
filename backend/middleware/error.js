71f313f9c4b731f9607f0664ca2c8c95d2385727
dipendra dewangan <dewangandipendradewangan@gmail.com>
2023-04-26 21:37:01 +0530

    added some more error situation validation

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -10,6 +10,26 @@ const errorMiddleware = (err, req, res, next)=>{
         err = new ErrorHandler(message, 400)
     }
 
+
+    // wrong mongodb duplicate key error
+    if(err.code === 11000){
+        const message = `Dublicate ${Object.keys(err.keyValue)} entrered!`
+        err = new ErrorHandler(message, 400);
+    }
+
+    // wrong JWT error
+    if(err.name === "JsonWebTokenError"){
+        const message = `Json web token is invalid, Try again!`
+        err = new ErrorHandler(message, 400)
+    }
+
+    
+    // JWT Expire error
+    if(err.name === "TokenExpireError"){
+        const message = `Json web token is Expire, Try again!`
+        err = new ErrorHandler(message, 400)
+    }
+
     res.status(err.statusCode).json({
         success : false,
         message : err.message
