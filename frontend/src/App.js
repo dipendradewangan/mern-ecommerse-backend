a2321ca19028114820ac59244b1ed29521181ede
dipendra dewangan <dewangandipendradewangan@gmail.com>
2023-06-17 23:01:45 +0530

    seted ui for navbar using webfontloader

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
 import Header from "./component/layout/Header.js"
 import './App.css';
 import { BrowserRouter } from 'react-router-dom'
+import { useEffect } from "react";
+import WebFont from "webfontloader";
 
 function App() {
+
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ['Roboto', 'Droid Sans', 'Chilanka']
+      }
+    })
+  }, [])
+
+
   return (
     <BrowserRouter>
-      <Header/>
+      <Header />
     </BrowserRouter>
   );
 }
