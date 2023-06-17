c6e0c7bd75cb1cb359967fe776ebb0702acf3aa3
dipendra dewangan <dewangandipendradewangan@gmail.com>
2023-06-17 20:44:31 +0530

    seted inital navbar design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
 import Header from "./component/layout/Header.js"
 import './App.css';
 import { BrowserRouter } from 'react-router-dom'
-import {ReactNavbar} from 'overlay-navbar'
 
 function App() {
   return (
     <BrowserRouter>
-      <ReactNavbar/>
-
+      <Header/>
     </BrowserRouter>
   );
 }
